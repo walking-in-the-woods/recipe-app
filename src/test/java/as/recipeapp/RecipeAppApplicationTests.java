c182e9a4730dd059ad03fa1e142cc88eafcd8e64
Alex S <as202009261140@gmail.com>
2021-02-20 15:48:54 +0300

    Init

---
+++ src/test/java/as/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package as.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
