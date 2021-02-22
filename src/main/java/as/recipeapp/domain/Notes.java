4c3358a208e6c80d6cba3a47a293a6ebf72b0954
Alex S <as202009261140@gmail.com>
2021-02-22 10:28:52 +0300

    Added domain

---
+++ src/main/java/as/recipeapp/domain/Notes.java
@@ -0,0 +1,48 @@
+package as.recipeapp.domain;
+
+/*
+    @Lob    - an annotation for large objects
+
+    In relational databases we can use CLOBS for character large objects or BLOBS for binary large objects.
+    In Hibernate and JPA the size limitation is 255 characters.
+*/
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
