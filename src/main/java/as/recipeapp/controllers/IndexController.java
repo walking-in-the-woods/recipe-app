a9368d66afdbe25a8c993066858acc878f8f6d1a
Alex S <as202009261140@gmail.com>
2021-02-22 20:57:54 +0300

    Refactoring. Added addIngredient() method

--- src/main/java/as/recipeapp/controllers/IndexController.java
+++ src/main/java/as/recipeapp/controllers/IndexController.java
@@ -23,27 +23,4 @@ public class IndexController {
 
         return "index";
     }
-
-//    private CategoryRepository categoryRepository;
-//    private UnitOfMeasureRepository unitOfMeasureRepository;
-//
-//    public IndexController(CategoryRepository categoryRepository,
-//                           UnitOfMeasureRepository unitOfMeasureRepository) {
-//        this.categoryRepository = categoryRepository;
-//        this.unitOfMeasureRepository = unitOfMeasureRepository;
-//    }
-//
-//    @RequestMapping({"", "/", "index"})
-//    public String getIndexPage() {
-//
-//        Optional<Category> categoryOptional = categoryRepository
-//                .findByDescription("American");
-//        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository
-//                .findByDescription("Teaspoon");
-//
-//        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-//        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
-//
-//        return "index";
-//    }
 }
