d8447efa565f0ba3808da8c9ef39a4c62b72487a
Alex S <as202009261140@gmail.com>
2021-02-22 13:57:24 +0300

    ManyToMany JoinTable

--- src/main/java/as/recipeapp/domain/Recipe.java
+++ src/main/java/as/recipeapp/domain/Recipe.java
@@ -1,5 +1,12 @@
 package as.recipeapp.domain;
 
+/*
+NOTE!
+
+To connect H2 database, copy the address from the Spring log at "Database available at '...'"
+o.s.b.a.h2.H2ConsoleAutoConfiguration    : H2 console available at '/h2-console'. Database available at '...'
+*/
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -17,17 +24,24 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredient;
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -107,5 +121,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
