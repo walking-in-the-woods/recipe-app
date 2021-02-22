d7e869ab96f8a06884f3ece03275901963ccd25f
Alex S <as202009261140@gmail.com>
2021-02-22 17:12:00 +0300

    Database Repositories and Query Methods

--- src/main/java/as/recipeapp/controllers/IndexController.java
+++ src/main/java/as/recipeapp/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package as.recipeapp.controllers;
 
+import as.recipeapp.domain.Category;
+import as.recipeapp.domain.UnitOfMeasure;
+import as.recipeapp.repositories.CategoryRepository;
+import as.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository,
+                           UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository
+                .findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository
+                .findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
