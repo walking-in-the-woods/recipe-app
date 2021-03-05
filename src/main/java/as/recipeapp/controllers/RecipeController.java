19845ca877c591b7975451049336e57ac619992b
Alex S <as202009261140@gmail.com>
2021-03-05 16:38:34 +0300

    Handling Number Format Exception

--- src/main/java/as/recipeapp/controllers/RecipeController.java
+++ src/main/java/as/recipeapp/controllers/RecipeController.java
@@ -55,12 +55,25 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
-        log.error("Handling not found exception");
+    public ModelAndView handleNotFoundException(Exception exception) {
+        log.error("Handling Not Found Exception");
         log.error(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception) {
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
--- src/test/java/as/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/as/recipeapp/controllers/RecipeControllerTest.java
@@ -51,12 +51,21 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void testGetRecipeNotFound() throws Exception {
+    public void testGetRecipeNotFoundException() throws Exception {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+        //when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
