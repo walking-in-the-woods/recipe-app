9d6670659553b0476d1eadd0ea170f42c5060e52
Alex S <as202009261140@gmail.com>
2021-03-05 17:03:49 +0300

    Added Controller Exception Handler

--- src/test/java/as/recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/as/recipeapp/controllers/ImageControllerTest.java
@@ -36,7 +36,9 @@ public class ImageControllerTest {
         MockitoAnnotations.openMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -94,5 +96,12 @@ public class ImageControllerTest {
         byte[] responseBytes = response.getContentAsByteArray();
 
         assertEquals(s.getBytes().length, responseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
--- src/test/java/as/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/as/recipeapp/controllers/RecipeControllerTest.java
@@ -32,7 +32,9 @@ public class RecipeControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.openMocks(this);
         controller = new RecipeController(recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
