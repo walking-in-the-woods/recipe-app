3e8c2e57f726b0ee225ecac43777311ed1e16f44
Alex S <as202009261140@gmail.com>
2021-04-18 10:15:09 +0300

    Changed imports in converters to org.springframework.lang.Nullable;

--- src/main/java/as/recipeapp/converters/CategoryCommandToCategory.java
+++ src/main/java/as/recipeapp/converters/CategoryCommandToCategory.java
@@ -2,9 +2,9 @@ package as.recipeapp.converters;
 
 import as.recipeapp.commands.CategoryCommand;
 import as.recipeapp.domain.Category;
-import com.sun.istack.Nullable;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
